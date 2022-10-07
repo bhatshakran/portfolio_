29965ae09f2d915f5ef6d8ab569382ea61add754
bhatshakran <bhatshakran@gmail.com>
2022-10-07 14:53:31 +0530

    pinned website animation integrated

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ module.exports = {
   theme: {
     screens: {
       sm: '470px',
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
+
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
     },
     extend: {
       colors: {
