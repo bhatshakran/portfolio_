d430096b778775121bf1d6dcf6635471a9ee4341
bhatshakran <bhatshakran@gmail.com>
2022-12-04 13:34:17 +0530

    changed url

--- app/routes/projects/$id.tsx
+++ app/routes/projects/$id.tsx
@@ -29,7 +29,7 @@ const a = {
   techUsed: ['graphql', 'remix.run', 'prisma'],
   about:
     'Ottelo is a hotel booking application website. Here you can book amazing suites, cottages and apartments in the top cities of the world like Dubai, London, Los Angeles, Toronto.The UI is very eloquent and the app is very easy to use.',
-  url: 'https://ottelo.herokuapp.com/',
+  url: 'https://ottelo.vercel.app/',
 };
 const c = {
   imgs: [
